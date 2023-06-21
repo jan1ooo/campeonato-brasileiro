6674052cb8ec2296b5b7900b372f343838b720b7
jan1ooo <filhojanio67@gmail.com>
2023-06-21 19:21:02 -0300

    Project Created

---
+++ src/test/java/com/jan1ooo/cbf/campeonatobrasileiro/CampeonatoBrasileiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.cbf.campeonatobrasileiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoBrasileiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
