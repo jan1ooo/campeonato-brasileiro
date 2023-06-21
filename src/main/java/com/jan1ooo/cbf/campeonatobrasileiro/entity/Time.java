f91ad5ebcde4f469e20d0b2cbf95104ae29187fd
jan1ooo <filhojanio67@gmail.com>
2023-06-21 19:43:09 -0300

    Atualização das entidades

---
+++ src/main/java/com/jan1ooo/cbf/campeonatobrasileiro/entity/Time.java
@@ -0,0 +1,26 @@
+package com.jan1ooo.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_time;
+
+    @Column(length = 20)
+    private String nome;
+
+    @Column(length = 4)
+    private String sigla;
+
+    @Column(length = 2)
+    private String uf;
+}
+
