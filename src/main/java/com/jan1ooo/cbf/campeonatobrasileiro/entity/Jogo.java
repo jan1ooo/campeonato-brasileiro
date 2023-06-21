48ba6311b650e9585ae34d9457e8aa15cf579cd7
jan1ooo <filhojanio67@gmail.com>
2023-06-21 19:43:09 -0300

    Criando as entidades

---
+++ src/main/java/com/jan1ooo/cbf/campeonatobrasileiro/entity/Jogo.java
@@ -0,0 +1,16 @@
+package com.jan1ooo.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Jogo {
+
+    @Id
+    private Time timeCasa;
+    private Time timeVisitante;
+    private Integer golsTimeCasa;
+    private Integer golsTimeVisitante;
+}
---
+++ src/main/java/com/jan1ooo/cbf/campeonatobrasileiro/entity/Time.java
@@ -0,0 +1,8 @@
+package com.jan1ooo.cbf.campeonatobrasileiro.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+public class Time {
+}
