2289293ec16fa4420aba30524a1ac98915237316
jan1ooo <filhojanio67@gmail.com>
2023-06-23 19:05:33 -0300

    Implementando Swagger

--- src/main/java/com/jan1ooo/cbf/campeonatobrasileiro/controller/TimeController.java
+++ src/main/java/com/jan1ooo/cbf/campeonatobrasileiro/controller/TimeController.java
@@ -2,14 +2,14 @@ package com.jan1ooo.cbf.campeonatobrasileiro.controller;
 
 import com.jan1ooo.cbf.campeonatobrasileiro.entity.Time;
 import com.jan1ooo.cbf.campeonatobrasileiro.service.TimeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Tutorial", description = "Tutorial management APIs")
 @RestController
 @RequestMapping("/api/times")
 public class TimeController {
@@ -20,5 +20,10 @@ public class TimeController {
     @GetMapping
     public ResponseEntity<List<Time>> getTimes() {
         return ResponseEntity.ok().body(service.listarTimes());
+    }
+    
+    @PostMapping
+    public ResponseEntity<Time> postTime(@RequestBody Time time) {
+        return ResponseEntity.status(201).body(service.cadastrarTime(time));
     }
 }
